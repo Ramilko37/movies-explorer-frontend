14bb7a9396108e634853af1e20317f84f82701de
Ramil Galyamdin <galyamdin@gmail.com>
2021-08-25 22:56:38 +0300

    Add files via upload

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Portfolio.css'
+
+const Portfolio = () => {
+    return(
+        <section className="portfolio section">
+                    <h2 className="portfolio__title">Портфолио</h2>
+                    <div className="portfolio__wrapper">
+                        <div className="portfolio__item">
+                            <a href="" className="portfolio__link">Статичный сайт</a>
+                        </div>
+                    </div>
+                </section>
+    )
+}
+
+export default Portfolio;
