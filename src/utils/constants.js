48a1f271e0f31365fcee92cf6343ae12a7bbe2ed
Ramil Galyamdin <galyamdin@gmail.com>
2021-09-05 17:53:09 +0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,5 @@ export const pcWidth = 1280;
 export const tabletWidth = 780;
 export const phoneWidth = 500;
 export const DURATION_MOVIE = 40;
-export const BASE_URL = "http://localhost:3005";
+export const BASE_URL = "https://api.ramilko-diplom.nomoredomains.club";
 export const URL_NOMOREPATIES = "https://api.nomoreparties.co";
